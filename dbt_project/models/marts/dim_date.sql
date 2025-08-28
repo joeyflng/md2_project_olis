1d371ee2127924a77366ff4d3dd3ab01f3e1720c
joey <joeyflng@gmail.com>
2025-08-28 12:59:59 +0800

    remove redundant files

--- dbt_project/models/marts/dim_date.sql
+++
@@ -1,9 +0,0 @@
-
-{{ config(materialized='table') }}
-SELECT DISTINCT
-    DATE(order_ts) AS date,
-    EXTRACT(YEAR FROM order_ts) AS year,
-    EXTRACT(MONTH FROM order_ts) AS month,
-    EXTRACT(DAY FROM order_ts) AS day,
-    EXTRACT(DAYOFWEEK FROM order_ts) AS day_of_week
-FROM {{ ref('stg_orders') }}
